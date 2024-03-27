5f179320a1072036058d9f9ccb5e3cfb529bcbf9
banya <bjgeumgang@gmail.com>
2024-03-27 22:19:01 +0900

    feat: json페이지 추가, 제품카드페이지 생성

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F5e%2F52%2F5e5292ccd68733153ef92868e2f1bd0b9835a6a9.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D" />
+      <div>Conscious choice</div>
+      <div>벨티드 트윈 코드</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
