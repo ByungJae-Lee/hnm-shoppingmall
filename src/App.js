c70c9480785c2d098930169160a034a2bd643703
banya <bjgeumgang@gmail.com>
2024-03-28 13:02:28 +0900

    feat: json과 파일 연결작업

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
