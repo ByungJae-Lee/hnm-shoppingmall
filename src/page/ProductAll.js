a901668e728f4293ff919294f0f57d1cf2bfd20a
banya <bjgeumgang@gmail.com>
2024-03-28 18:06:51 +0900

    feat: json서버 배포용으로 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import ProductCard from "../component/ProductCard";
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = `http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/ByungJae-Lee/hnm-shoppingmall/products`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
